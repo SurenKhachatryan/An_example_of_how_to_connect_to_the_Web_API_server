c4421fff9b736ed95a0af198843dd1a606585090
Suren Khachatryan <surench94@gmail.com>
2019-02-07 22:24:26 +0400

    How To Connect To The Web API Server

--- How_to_connect_to_the_Web_API_server/UsersDataWebAPI/Controllers/UsersController.cs
+++ How_to_connect_to_the_Web_API_server/UsersDataWebAPI/Controllers/UsersController.cs
@@ -10,10 +10,94 @@ namespace UsersDataWebAPI.Controllers
 {
     public class UsersController : ApiController
     {
-        private UserContext db = new UserContext();
+        UserContext db = new UserContext();
+        User user = new User();
+
+        /// <summary>
+        /// Возвращает всех пользователей
+        /// </summary>
+        [HttpGet]
         public IEnumerable<User> GetAllUser()
         {
             return db.Users;
+        }
+
+
+        /// <summary>
+        /// Возвращает пользователя по определенному Id
+        /// </summary>
+        [HttpGet]
+        public HttpResponseMessage GetUser(int id)
+        {
+            user = db.Users.Where(u => u.UserId == id).FirstOrDefault();
+
+            if (user != null)
+                return Request.CreateResponse(HttpStatusCode.OK, user);
+            else
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Not Found - {id}");
+        }
+
+
+        /// <summary>
+        /// Саздает нового пользователя
+        /// </summary>
+        [HttpPost]
+        public HttpResponseMessage CreateNewUser([FromBody]User user)
+        {
+            try
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.Created);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+
+        /// <summary>
+        /// Обнавляет данные пользователя
+        /// </summary>
+        [HttpPut]
+        public HttpResponseMessage UpdateUser(int id, [FromBody] User user)
+        {
+            this.user = db.Users.Where(u => u.UserId == id).FirstOrDefault();
+            if (user != null)
+            {
+                try
+                {
+                    this.user.Age = user.Age;
+                    this.user.Fitst_Name = user.Fitst_Name;
+                    this.user.Last_Name = user.Last_Name;
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                catch (Exception)
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError);
+                }
+            }
+            else
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+
+        /// <summary>
+        /// Удаляет пользователя
+        /// </summary>
+        [HttpDelete]
+        public HttpResponseMessage DeleteUser(int id)
+        {
+            user = db.Users.Where(u => u.UserId == id).FirstOrDefault();
+            if (user != null)
+            {
+                db.Users.Remove(user);
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Not Found - {id}");
         }
     }
 }
